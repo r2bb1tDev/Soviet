f8e1266001b8f09ad9a220e1c35aee9d66d68707
Soviet Dev <soviet@dev.local>
2026-04-04 13:49:26 +0200

    Temporarily remove updater code to fix builds

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useEffect } from 'react'
 import { listen } from '@tauri-apps/api/event'
 import { invoke } from '@tauri-apps/api/core'
 // import { checkUpdate, installUpdate } from '@tauri-apps/api/updater'
